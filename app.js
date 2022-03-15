ae41bdc980b499798c846b3645d4244eb4bf9991
Kwon Kyoung min <lanos5019@ajou.ac.kr>
2022-03-15 22:36:57 +0900

    3.15commit

--- app.js
+++ app.js
@@ -5,19 +5,27 @@ const result = document.querySelector(".result");
 const h4 = result.querySelector("h4");
 const h2 = result.querySelector("h2");
 
+
 function startGame(event){
     event.preventDefault();
-    const resultNum = Math.ceil((Math.random()*parseInt(randomNum.value)));
-    if(parseInt(yourNum.value) > parseInt(randomNum.value)){
+
+    if((parseInt(yourNum.value) < 0) || (parseInt(randomNum.value) < 0)){
         printWrongNum();
-    }else{
-        printresult(resultNum);
+    }
+    else{
+        const resultNum = Math.ceil((Math.random()*parseInt(randomNum.value)));
+        if(parseInt(yourNum.value) > parseInt(randomNum.value)){
+            printWrongNum();
+        }else{
+            printresult(resultNum);
+        }
     }
 }
 function printWrongNum(){
     h4.innerText = "You enter the wrong number!"
     h2.innerText = "";
     result.classList.remove("hidden");
+    result.classList.add("text-focus-in");
 }
 function printresult(resultNum){
     h4.innerText = 
@@ -28,8 +36,15 @@ function printresult(resultNum){
         h2.innerText = "You lost!"
     }
     result.classList.remove("hidden");
-
+    result.classList.add("text-focus-in");
 }
 
 
 randomInput.addEventListener("submit", startGame);
+
+function earase(){
+    this.placeholder = "";
+}
+function print(string){
+    this.placeholder = string;
+}
